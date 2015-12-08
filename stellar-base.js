926a0d705cf9e8b3f4edd17059c6c5f233105716
Travis CI <travis@stellar.org>
2015-12-08 13:57:37 +0000

    v0.4.19

--- stellar-base.js
+++ stellar-base.js
@@ -19608,38 +19608,8 @@ var StellarBase =
 /* 16 */
 /***/ function(module, exports) {
 
-	
-	/**
-	 * isArray
-	 */
-
-	var isArray = Array.isArray;
-
-	/**
-	 * toString
-	 */
-
-	var str = Object.prototype.toString;
-
-	/**
-	 * Whether or not the given `val`
-	 * is an array.
-	 *
-	 * example:
-	 *
-	 *        isArray([]);
-	 *        // > true
-	 *        isArray(arguments);
-	 *        // > false
-	 *        isArray('');
-	 *        // > false
-	 *
-	 * @param {mixed} val
-	 * @return {bool}
-	 */
-
-	module.exports = isArray || function (val) {
-	  return !! val && '[object Array]' == str.call(val);
+	module.exports = Array.isArray || function (arr) {
+	  return Object.prototype.toString.call(arr) == '[object Array]';
 	};
 
 
@@ -29493,7 +29463,7 @@ var StellarBase =
 	     *
 	     * * `native`
 	     * * `credit_alphanum4`
-	     * * `credit_alphanum4`
+	     * * `credit_alphanum12`
 	     *
 	     * @see [Assets concept](https://www.stellar.org/developers/learn/concepts/assets.html)
 	     * @returns {string}
